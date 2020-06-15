3a697f76dbaf04b6aa9482759bf86196a641701a
jiyoung <jiyoung-park@live.nmit.ac.nz>
2020-06-15 13:07:39 +1200

    lab4

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-function binary (num){
+function binary (num){ //declare function named 'binary
  
- if (num < 1024 && num > 0){
-  let a = num.toString(2);
-  console.log(a);
+ if (num < 1024 && num > 0){ //conditional syntax: if 
+    // if parameter named num is under 1024 and greater than 0
+  var digit = num.toString(2) //converts num which is decimal to binary 
+  console.log(digit); //print the value
  }
 }
 binary(5)
+
